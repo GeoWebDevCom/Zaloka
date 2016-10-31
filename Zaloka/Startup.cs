0b71cca38cacc4ed6689f91eaca47f8b84ec035a
HaoEduu <hao.tran@eduu.vn>
2016-10-31 12:53:22 +0700

    Add project files.

---
+++ Zaloka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zaloka.Startup))]
+namespace Zaloka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
